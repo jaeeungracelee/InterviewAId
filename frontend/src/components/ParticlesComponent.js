bc86e0b2c247216b1e969806deadc89a02495226
Daniel Pan <57362494+daniel-panhead@users.noreply.github.com>
2024-06-23 01:00:47 -0700

    Finish implementing react particles :)

--- frontend/src/components/ParticlesComponent.js
+++ frontend/src/components/ParticlesComponent.js
@@ -1,23 +1,43 @@
-import React from 'react';
-import Particles from '@tsparticles/react';
-import particlesConfig from '../components/particlesConfig';
+import React, { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import particlesConfig from "../components/particlesConfig";
+import { loadSlim } from "@tsparticles/slim";
 
 const ParticlesComponent = () => {
-  console.log('ParticlesComponent rendered');
-  console.log('particlesConfig:', particlesConfig);
+  console.log("ParticlesComponent rendered");
+  console.log("particlesConfig:", particlesConfig);
+
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
 
   return (
-    <Particles
-      id="tsparticles"
-      options={particlesConfig}
-      style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-      }}
-    />
+    init && (
+      <Particles
+        id="tsparticles"
+        options={particlesConfig}
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      />
+    )
   );
 };
 
